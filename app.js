3d6ddae099c8757ad06f55f879e12640b37d8eb6
g.dini <g.dini@tdnet.it>
2025-08-03 15:17:38 +0200

    ghiera solidale

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function drawOverlay() {
 function updateCompass(alpha) {
   const rot = 360 - alpha;
   needleRed.style.transform = `rotate(${rot}deg)`;
-  overlay.style.transform = `rotate(${rot}deg)`;
+  //overlay.style.transform = `rotate(${rot}deg)`; // ghiera solidale
   //arrow.style.transform = `rotate(${rot}deg)`; // freccia solidale
   direction.textContent = `Direzione: ${Math.round(alpha)}°`;
 }
